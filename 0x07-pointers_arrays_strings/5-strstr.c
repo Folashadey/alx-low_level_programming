5ebbb97901c6adb70a9dc171c0836312666be124
Folashadey <patienceakogun4@gmail.com>
2022-03-28 12:28:47 -0700

    Fixed commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -30,36 +30,4 @@ char *_strstr(char *haystack, char *needle)
 		haystack = res + 1;
 	}
 	return (NULL);
-}#include "main.h"
-#include <stdio.h>
-
-/**
- * _strstr -  function that locates a substring.
- *
- * @haystack: initial string
- * @needle: substring
- * Return: char
- */
-
-char *_strstr(char *haystack, char *needle)
-{
-	char *res;
-	char *aux;
-
-	while (*haystack != '\0')
-	{
-		res = haystack;
-		aux = needle;
-
-		while (*aux == *haystack && *aux != '\0'
-		       && *haystack != '\0')
-		{
-			haystack++;
-			aux++;
-		}
-		if (*aux == '\0')
-			return (res);
-		haystack = res + 1;
-	}
-	return (NULL);
 }
