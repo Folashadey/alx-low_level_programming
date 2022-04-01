66be9ea079ce07710995408670da2a310244a9b0
Folashadey <patienceakogun4@gmail.com>
2022-04-01 07:17:12 -0700

    Fixed commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,18 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - multiply 2 numbers passed to main, or Error
- * @argc: argument count
- * @argv: argument vector
- * Return: 1 if error, 0 if function runs correctly
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
-	(void) argc;
-
-	if (argv[1] && argv[2])
+	if (argc != 3)
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-		return (0);
-	}
-	else
 		printf("Error\n");
-
-	return (1);
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
