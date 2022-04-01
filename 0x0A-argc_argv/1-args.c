7f8c1b6f8547bf33becded79e9d46eeb6324a63d
Folashadey <patienceakogun4@gmail.com>
2022-04-01 06:48:24 -0700

    Fixed commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - print the num of args you passed to it
+ *
+ *   * @argc: argument count
+ *
+ *    * @argv: argu:
+ *      */
+
+
+
+int main(int argc, char *argv[])
+
+{
+
+	(void) argv;
+
+
+
+	printf("%d\n", argc - 1);
+
+
+
+	return (0);
+
+}
